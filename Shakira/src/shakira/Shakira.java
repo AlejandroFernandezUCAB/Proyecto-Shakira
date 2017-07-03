7796293e0210359355eb90d30ac6187e1b66789f
AlejandroFernandezUCAB <pafernandez.14@est.ucab.edu.ve>
2017-07-03 18:44:16 -0400

    Actualizando el proyecto

--- Shakira/src/shakira/Shakira.java
+++ Shakira/src/shakira/Shakira.java
@@ -15,7 +15,8 @@ public class Shakira {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ConsolaShakira consola = new ConsolaShakira();
+        consola.show();
     }
     
 }
