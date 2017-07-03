3d5443fe65c45d52396500a993ce06cf91d982b0
AlejandroFernandezUCAB <pafernandez.14@est.ucab.edu.ve>
2017-07-03 19:16:48 -0400

    Agregando la misma interfaz a todos

--- Shakira-Servidor-Central/src/shakira/servidor/central/Controladora.java
+++ Shakira-Servidor-Central/src/shakira/servidor/central/Controladora.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package shakira;
+package shakira.servidor.secundario;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
