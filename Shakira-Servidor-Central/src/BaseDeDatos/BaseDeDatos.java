905ea188f7fb5b427b1401dcca3e67cd300538d2
AlejandroFernandezUCAB <pafernandez.14@est.ucab.edu.ve>
2017-07-17 15:04:23 -0400

    Arreglando Estructura del proyecto
    Arreglé la estructura dle proyecto para tener todo más ordenado ya que el servidor secundario será cliente-servidor

---
+++ Shakira-Servidor-Central/src/BaseDeDatos/BaseDeDatos.java
@@ -0,0 +1,185 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaseDeDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Alejandro Fernandez
+ */
+public class BaseDeDatos {
+
+public static String driver = "org.postgresql.Driver";
+public static String connectString = "jdbc:postgresql://localhost:5432/Shakira-Servidor-Central";
+public static String user = "redes2";
+public static String password = "redes2";
+    
+
+    /**
+     * Constructor vacio para crear el objeto
+     */
+    public BaseDeDatos(){
+            
+    }
+/**
+ * Metodo que crea la conexion en la base de datos
+     * @param direccionIp Direccion ip cliente
+     * @param puertoEscucha Puerto escucha del cliente
+     * @return devuelve 1 si se registró correctamente, si hay error 0
+ */
+
+    public int agregarUsuarioBDD(String direccionIp, int puertoEscucha){
+        
+        String stm = "INSERT INTO CLIENTE(ipcliente, puertoEscucha) VALUES(?, ?)";
+        PreparedStatement pst = null;
+        Connection con=null;
+        //Se abren las conexiones a la BDD y e guarda el usuario,
+        if(verificarInscripcionUsuario(direccionIp) == false){
+            try{
+                Class.forName(driver);
+                con = DriverManager.getConnection(connectString, user , password);
+                pst = con.prepareStatement(stm);
+                pst.setString(1, direccionIp);
+                pst.setInt(2, puertoEscucha);                    
+                pst.executeUpdate();
+
+                } catch ( SQLException | ClassNotFoundException e ){
+
+                    System.out.println("No se inscribio al usuario: " + direccionIp);
+                    return 0;
+
+                } finally {
+                // Con el finally se cierran todas las conexiones los con, pst;
+                    try {
+
+                        if (pst != null) {
+                            pst.close();
+                        }
+                        if (con != null) {
+                            con.close();
+                        }
+
+                    } catch (SQLException ex) {
+
+                        System.out.println(ex);                
+                        return 1;
+                    }
+
+            }
+            System.out.println("Se inscribio al usuario: " + direccionIp);
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    
+    public boolean verificarInscripcionUsuario(String ip){
+        boolean suiche = false;
+        try{
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(connectString, user , password);
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT ipcliente FROM cliente");
+
+            while (rs.next()){
+                
+                System.out.println("Ip: " + rs.getString("ipcliente"));
+                if(rs.getString("ipcliente").contains(ip)){
+                    suiche = true;
+                }
+            }
+
+                stmt.close();
+                con.close();
+
+            }catch ( Exception e ){
+                 System.out.println(e.getMessage());
+            }
+        
+            return suiche;
+        }
+
+    public int agregarServidorBDD(String direccionIp, int puertoEscucha) {
+        String stm = "INSERT INTO SERVIDOR (ipservidor, puertoEscucha) VALUES(?, ?)";
+        PreparedStatement pst = null;
+        Connection con=null;
+        //Se abren las conexiones a la BDD y se guarda la BDD, además se verifica que no haya un servidor con la misma Ip
+        if( verificarInscripcionDeServidor( direccionIp ) == false ){
+            try{
+                Class.forName(driver);
+                con = DriverManager.getConnection(connectString, user , password);
+                pst = con.prepareStatement(stm);
+                pst.setString(1, direccionIp);
+                pst.setInt(2, puertoEscucha);                    
+                pst.executeUpdate();
+
+                } catch ( SQLException | ClassNotFoundException e ){
+
+                    System.out.println("No se inscribio al servidor: " + direccionIp);
+                    return 0;
+
+                } finally {
+                // Con el finally se cierran todas las conexiones los con, pst;
+                    try {
+
+                        if (pst != null) {
+                            pst.close();
+                        }
+                        if (con != null) {
+                            con.close();
+                        }
+
+                    } catch (SQLException ex) {
+
+                        System.out.println(ex);                
+                        return 1;
+                    }
+
+            }
+            System.out.println("Se inscribio al servidor: " + direccionIp);
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    
+    public boolean verificarInscripcionDeServidor(String ip){
+         boolean suiche = false;
+        try{
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(connectString, user , password);
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT ipservidor FROM servidor");
+
+            while (rs.next()){
+                
+                System.out.println("Ip: " + rs.getString("ipservidor"));
+                if(rs.getString("ipservidor").contains(ip)){
+                    suiche = true;
+                }
+            }
+
+                stmt.close();
+                con.close();
+
+            }catch ( Exception e ){
+                 System.out.println(e.getMessage());
+            }
+        
+            return suiche;
+    }
+}
+
+
+
---
+++ Shakira-Servidor-Secundario/src/LogicaCliente/Controladora.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LogicaCliente;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author pedro
+ */
+public class Controladora extends Thread{
+    private String comando;
+
+      /**
+    * Constructor para inicializar el objeto
+    * @param comando comando recibido por el cliente
+    */
+    Controladora(String comando) {
+        this.comando = comando;
+    }
+    
+    
+    @Override
+    public void run(){
+        
+        if( comando.trim().contains("inscribir")){
+            
+            SocketConexionSecundario socket = new SocketConexionSecundario();
+            socket.inscribirServidor();
+            
+        }else if( comando.trim().contains( "videos_descargando" ) ){
+            
+        }else if ( comando.trim().contains( "videos_descargados" ) ){
+        
+        }else{
+            System.err.println( "Servidor Secundario > Error en el comando" );
+        }
+        
+    }
+}
