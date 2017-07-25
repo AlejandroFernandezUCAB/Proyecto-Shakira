8e2afe963c041877132411029afb81d619827d4f
AlejandroFernandezUCAB <pafernandez.14@est.ucab.edu.ve>
2017-07-25 00:14:38 -0400

    Arreglo tonto en la BDD

--- Shakira-Servidor-Secundario/src/BaseDeDatos/BaseDeDatos.java
+++ Shakira-Servidor-Secundario/src/BaseDeDatos/BaseDeDatos.java
@@ -160,7 +160,7 @@ public class BaseDeDatos {
 
     public String rutaVideo(String nombreVideo) {
         String retorno=null;
-        nombreVideo = nombreVideo.substring(11);
+        nombreVideo = nombreVideo.substring(10);
         try{
             Class.forName(driver);
             Connection con = DriverManager.getConnection(connectString, user , password);
