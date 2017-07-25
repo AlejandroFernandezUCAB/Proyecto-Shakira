4e5ea4d08606438f383ccc448fb672c89767d726
Unknown <gianfrancoverrocchi@gmail.com>
2017-07-24 20:23:59 -0400

    prueba 1

--- Shakira-Servidor-Secundario/src/LogicaServidor/SocketConexionSecundario.java
+++ Shakira-Servidor-Secundario/src/LogicaServidor/SocketConexionSecundario.java
@@ -5,12 +5,18 @@
  */
 package LogicaServidor;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 
 /**
@@ -32,6 +38,65 @@ public class SocketConexionSecundario extends Thread{
             while (true) {  
                 String str = entrada.readLine();
                 System.out.println(str);
+                salida.println("Servidor Secundario > enviando video");
+                
+                //--------------envio el archivo------------
+                        //IP de la maquina que recibira el archivo
+                        InetAddress direccion = ss.getInetAddress();
+                        //No hace falta que creemos otro socket
+                        //usamos esta misma conexion
+
+                        String nombreArchivo = str.substring(9);//quito: descargar_
+
+                        // Creamos el archivo que vamos a enviar
+                        File archivo = new File( nombreArchivo );
+
+                        // Obtenemos el tamaño del archivo
+                        int tamañoArchivo = ( int )archivo.length(); 
+
+                        /*
+                        
+                        // Creamos el flujo de salida, este tipo de flujo nos permite 
+                        // hacer la escritura de diferentes tipos de datos tales como
+                        // Strings, boolean, caracteres y la familia de enteros, etc.
+                        DataOutputStream dos = new DataOutputStream( ss.getOutputStream() );
+
+                        System.out.println( "Enviando Archivo: "+archivo.getName() );
+
+                        // Enviamos el nombre del archivo 
+                        dos.writeUTF( archivo.getName() );
+
+                        // Enviamos el tamaño del archivo
+                        dos.writeInt( tamañoArchivo );
+
+
+                         // Creamos flujo de entrada para realizar la lectura del archivo en bytes
+                        FileInputStream fis = new FileInputStream( nombreArchivo );
+                        BufferedInputStream bis = new BufferedInputStream( fis );
+
+                        // Creamos el flujo de salida para enviar los datos del archivo en bytes
+                        BufferedOutputStream bos = new BufferedOutputStream( ss.getOutputStream()          );
+
+                        // Creamos un array de tipo byte con el tamaño del archivo 
+                        byte[] buffer = new byte[ tamañoArchivo ];
+
+                        // Leemos el archivo y lo introducimos en el array de bytes 
+                        bis.read( buffer ); 
+
+                        // Realizamos el envio de los bytes que conforman el archivo
+                        for( int x = 0; x < buffer.length; x++ )
+                        {
+                            bos.write( buffer[ x ] ); 
+                        } 
+
+                        System.out.println( "Archivo Enviado: "+archivo.getName() );
+                        // Cerramos socket y flujos
+                        bis.close();
+                        bos.close();    
+                    */
+                
+                //--------------termino envio---------------
+                
                   break;
             }
           }catch (IOException e) {
